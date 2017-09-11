aadbf9331a883c513c21982f8a578b78d627845f
n4cl <devn4cl@gmail.com>
2017-09-11 11:53:45 +0900

    Add setup.sh
    To intall packages

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install packages
+pacman -S --noconfirm git
+pacman -S --noconfirm vim
+pacman -S --noconfirm mingw-w64-x86_64-toolchain
+pacman -S --noconfirm python2 python2-pip
+pacman -S --noconfirm gcc
+
